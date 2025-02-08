47c77996fae6dfc6b6a055bc52740dc4ee5af421
Biprangshu Das <dasbiprangshu@gmail.com>
2025-02-08 20:20:10 +0530

    Implemented the NavGraoh

--- app/src/main/java/com/biprangshu/pokedex/MainActivity.kt
+++ app/src/main/java/com/biprangshu/pokedex/MainActivity.kt
@@ -4,44 +4,53 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.biprangshu.pokedex.ui.theme.PokeDexTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PokeDexTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val navController= rememberNavController()
+                NavHost(navController = navController, startDestination = "pokemon_list_screen"){
+                    composable("pokemon_list_screen"){
+
+                    }
+
+                    composable("pokemon_detail_screen/{dominantColor}/{pokemonName}", arguments = listOf(
+                        navArgument("dominantColor"){
+                            type= NavType.IntType
+                        },
+                        navArgument("pokemonName"){
+                            type= NavType.StringType
+                        }
+
                     )
+                    ){
+                        val dominantColor = remember {
+                            val color= it.arguments?.getInt("dominantColor")
+                            color?.let{
+                                Color(it)
+                            }?: Color.White
+                        }
+                        val pokemonName = remember {
+                            it.arguments?.getString("pokemonName")
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PokeDexTheme {
-        Greeting("Android")
-    }
-}
