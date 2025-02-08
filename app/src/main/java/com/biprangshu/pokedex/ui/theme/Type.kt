ad1cb49dc4ce6407933395a072c3886a6387e59b
Biprangshu Das <dasbiprangshu@gmail.com>
2025-02-08 19:38:26 +0530

    Added the Fonts and the styles for the app

--- app/src/main/java/com/biprangshu/pokedex/ui/theme/Type.kt
+++ app/src/main/java/com/biprangshu/pokedex/ui/theme/Type.kt
@@ -2,9 +2,32 @@ package com.biprangshu.pokedex.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.biprangshu.pokedex.R
+
+
+val Roboto = FontFamily(
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_bold, FontWeight.Bold),
+)
+
+val RobotoCondensed = FontFamily(
+    Font(R.font.roboto_condensed_light, FontWeight.Light),
+    Font(R.font.roboto_condensed_regular, FontWeight.Normal),
+)
+
+val RubricMono= FontFamily(
+    Font(R.font.rubrikmono, FontWeight.Normal)
+)
+
+val RussoOne= FontFamily(
+    Font(R.font.russoone_regular, FontWeight.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
