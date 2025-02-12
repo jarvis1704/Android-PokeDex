11699f8301b1f698b02d5f590dfa564d25bf09b6
Biprangshu Das <dasbiprangshu@gmail.com>
2025-02-12 20:38:31 +0530

    Finalized AppModule

--- app/src/main/java/com/biprangshu/pokedex/di/AppModule.kt
+++ app/src/main/java/com/biprangshu/pokedex/di/AppModule.kt
@@ -1,10 +1,34 @@
 package com.biprangshu.pokedex.di
 
+import com.biprangshu.pokedex.remote.PokeApi
+import com.biprangshu.pokedex.respository.PokemonRepository
+import com.biprangshu.pokedex.util.Constants.BASE_URL
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+
+    @Singleton
+    @Provides
+    fun ProvidePokemonRepository(
+        api: PokeApi,
+    )= PokemonRepository(api)
+
+    @Singleton
+    @Provides
+    fun ProvidePokeApi(): PokeApi{
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PokeApi::class.java)
+    }
 }
